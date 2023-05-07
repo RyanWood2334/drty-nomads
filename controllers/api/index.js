ae88ef09db5df4f37b8772349c5cf745c657513f
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-05-06 22:53:13 -0700

    add boilerplate for api controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+// Import controllers for all models here
+
+// Users Controller goes here
+
+// Passport Controller goes here
+
+// Photo Controller goes here
+
+// Place Controller goes here
+
+// Stamp Controller goes here
+
+
+module.exports = router;
