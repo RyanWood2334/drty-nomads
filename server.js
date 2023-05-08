4724ea7f1ede76792ff498eb06a321bdde52d57f
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-05-08 15:14:28 -0700

    updates to seeds, server, and config

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
