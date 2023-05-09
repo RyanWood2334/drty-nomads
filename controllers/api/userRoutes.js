8169e76d8b478fe88449e17fb1fef35d93ff4c84
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-09 08:11:05 -0700

    fixed the bad merge

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,21 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../../models");
 
-<<<<<<< HEAD
-// const router = require('express').Router();
-// const { User } = require('../../models');
-
 router.post("/", async (req, res) => {
-=======
-router.post('/', async (req, res) => {
-
   const newUser = {
     first_name: req.body.name,
     user_name: req.body.username,
     password: req.body.password,
-  }
+  };
 
->>>>>>> dev
   try {
     const userData = await User.create(newUser);
 
@@ -31,82 +23,38 @@ router.post('/', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-router.post("/login", async (req, res) => {
-  try {
-    // Find the user who matches the posted e-mail address
-    const userData = await User.findOne({
-      where: { username: req.body.username },
-    });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-=======
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // Find the user who matches the posted e-mail address
-  User.findOne({ where: { user_name: req.body.username } }).then((userData) => {
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email ' });
->>>>>>> dev
-      return;
-    }
+  User.findOne({ where: { user_name: req.body.username } })
+    .then((userData) => {
+      if (!userData) {
+        res.status(400).json({ message: "Incorrect email " });
+        return;
+      }
 
-  // Verify the posted password with the password store in the database
-  const validPassword = userData.checkPassword(req.body.password);
+      // Verify the posted password with the password store in the database
+      const validPassword = userData.checkPassword(req.body.password);
 
-<<<<<<< HEAD
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
+      if (!validPassword) {
+        res.status(400).json({ message: "Incorrect password" });
+        return;
+      }
 
-    // Create session variables based on the logged in user
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      // Create session variables based on the logged in user
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
 
-      res.json({ user: userData, message: "You are now logged in!" });
+        res.json({ user: userData, message: "You are now logged in!" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
-  } catch (err) {
-    res.status(400).json(err);
-=======
-  if (!validPassword) {
-    res
-      .status(400)
-      .json({ message: 'Incorrect password' });
-    return;
->>>>>>> dev
-  }
-
-  // Create session variables based on the logged in user
-  req.session.save(() => {
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
-    
-    res.json({ user: userData, message: 'You are now logged in!' });
-  });
-
-
-  }).catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  })
-
 });
 
-<<<<<<< HEAD
-// router.post to create new user.
-
 router.post("/logout", (req, res) => {
-=======
-router.post('/logout', (req, res) => {
->>>>>>> dev
   if (req.session.logged_in) {
     // Remove the session variables
     req.session.destroy(() => {
