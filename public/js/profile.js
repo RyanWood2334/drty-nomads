e4b5286bd4c8c04e156d71ad4b814e165c9221a8
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-09 15:21:05 -0700

    build a new stamp route, still need config

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,62 +1,63 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const destination_name = document.querySelector('#project-name').value.trim();
-  const photo = document.querySelector('#project-funding').value.trim();
-  const destination_notes = document.querySelector('#project-desc').value.trim();
+  const destination_name = document.querySelector("#project-name").value.trim();
+  const photo = document.querySelector("#project-funding").value.trim();
+  const destination_notes = document
+    .querySelector("#project-desc")
+    .value.trim();
 
-  if (destination_name && destination_notes ) {
+  if (destination_name && destination_notes) {
     const response = await fetch(`/api/stamps`, {
-      method: 'POST',
-      body: JSON.stringify({ destination_name, photo , destination_notes }),
+      method: "POST",
+      body: JSON.stringify({ destination_name, photo, destination_notes }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to create project');
+      alert("Failed to create project");
     }
   }
 };
 
-
-const deleteButtons = document.querySelectorAll('[data-id]');
-deleteButtons.forEach(button => {
-  button.addEventListener('click', async (event) => {
-    const id = event.target.getAttribute('data-id');
+const deleteButtons = document.querySelectorAll("[data-id]");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", async (event) => {
+    const id = event.target.getAttribute("data-id");
     const response = await fetch(`/api/stamps/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
       console.log(response.status);
-      alert('Failed to delete project');
+      alert("Failed to delete project");
     }
   });
 });
 
-
-
-const newStampBtn = document.querySelector('#new-stamp-btn');
-  const newStampFormContainer = document.querySelector('#new-stamp-form-container');
-  let n=0;
-  const showNewStampForm = () => {
-    if (n==0) {
-      newStampFormContainer.style.display = 'block';
-      n++;
-    } else {
-    newStampFormContainer.style.display = 'none';
+const newStampBtn = document.querySelector("#new-stamp-btn");
+const newStampFormContainer = document.querySelector(
+  "#new-stamp-form-container"
+);
+let n = 0;
+const showNewStampForm = () => {
+  if (n == 0) {
+    newStampFormContainer.style.display = "block";
+    n++;
+  } else {
+    newStampFormContainer.style.display = "none";
     n--;
-    }
-  };
-  
-  newStampBtn.addEventListener('click', showNewStampForm);
+  }
+};
+
+newStampBtn.addEventListener("click", showNewStampForm);
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+  .querySelector(".new-project-form")
+  .addEventListener("submit", newFormHandler);
